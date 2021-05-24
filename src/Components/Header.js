ff8f07021201d19ca11fcf9e9992e12e6d1ced8a
Cédric Bois <cedric.bois@uca.fr>
2021-05-24 22:00:37 +0200

    fix after merge

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,21 +4,32 @@ import { Navbar } from 'react-materialize'
 
 import imgbdla from './../img/bandeau.png'
 
-class Header extends Component {
-  render() {
-    return (
-      <div className='header'>
-        <Link className="unselectable" to='/'><img src={imgbdla} className='imgbdla' alt='' /></Link>
-        <Navbar className='navbar light-green darken-3'>
-          <Link className="nav-item unselectable" to="/">Accueil</Link>
-          <Link className="nav-item unselectable" to='/festival'>Festival</Link>
-          <Link className="nav-item unselectable" to='/vendredi'>Vendredi</Link>
-          <Link className="nav-item unselectable" to='/samedi'>Samedi</Link>
-          <Link className="nav-item unselectable" to='/infos'>Infos</Link>
-          <Link className="nav-item unselectable" to='/remerciements'>Remerciements</Link>
-          <Link className="nav-item unselectable" to='/contact'>Contact</Link>
-        </Navbar>
-      </div>
+const Header = () => {
+  return (
+    <div className="header">
+      <Link className="unselectable" to="/">
+        <img src={imgbdla} className="imgbdla" alt="" />
+      </Link>
+      <Navbar className="navbar light-green darken-3">
+        <Link className="nav-item unselectable" to="/">
+          Accueil
+        </Link>
+        <Link className="nav-item unselectable" to="/festival">
+          Festival
+        </Link>
+        <Link className="nav-item unselectable" to="/vendredi">
+          Vendredi
+        </Link>
+        <Link className="nav-item unselectable" to="/samedi">
+          Samedi
+        </Link>
+        <Link className="nav-item unselectable" to="/infos">
+          Infos
+        </Link>
+        <Link className="nav-item unselectable" to="/contact">
+          Contact
+        </Link>
+      </Navbar>
     </div>
   )
 }
